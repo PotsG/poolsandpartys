495ca111047ccc0b5a6de93ff9281aa05ba50e2a
RobotMargie <gregorypots32@gmail.com>
2020-07-31 23:58:48 +0200

    Final Invitation

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.clear();
     console.log('dikke feest');
-
-    const form = document.querySelector('#form');
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const form = e.target;
-
-        const formData = new FormData(form);
-        const name = formData.get('name');
-        const content = formData.get('content');
-
-        let obj = {
-            name,
-            content
-        };
-        console.log(obj);
-    };
-
-    form.addEventListener('submit', handleSubmit);
 });
